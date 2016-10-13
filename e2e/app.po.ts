8f0d845f88fde82815e4c9664c7ec68d0d9020da
Nargis <nargisshan@gmail.com>
2016-10-13 16:31:07 +0500

    Initial commit

---
+++ e2e/app.po.ts
@@ -0,0 +1,9 @@
+export class Angular2FirstAppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+
+  ClickMe(){
+    alert("Welcome");
+  }
+}
